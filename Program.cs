bb5f6032bcaae6381512aa72b8315ed5eb214295
Brian Mikinski <Brian.Mikinski@gmail.com>
2018-01-11 14:16:09 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpressionWorkBench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
